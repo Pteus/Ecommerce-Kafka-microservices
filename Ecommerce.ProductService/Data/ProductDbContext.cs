ff52d8991edb16f4b3abfa4e1c54cfe57c687793
Pedro Mateus <pedropmateus91@gmail.com>
2024-11-01 22:02:05 +0000

    initial commit

---
+++ Ecommerce.ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,23 @@
+using Ecommerce.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.ProductService.Data;
+
+public sealed class ProductDbContext : DbContext
+{
+    public ProductDbContext(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 1, Name = "Apple", Quantity = 20, Price = 10 });
+        modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 2, Name = "Orange", Quantity = 15, Price = 15 });
+        modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 2, Name = "Banana", Quantity = 5, Price = 20 });
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<ProductModel> Products { get; set; }
+}
